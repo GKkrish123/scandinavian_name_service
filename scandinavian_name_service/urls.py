1a4edd0f441ba2ed088b37741c4901f33ff15319
Gokulakrishnan <67182212+GKkrish123@users.noreply.github.com>
2025-05-22 09:51:01 +0000

    Update urls

--- scandinavian_name_service/urls.py
+++ scandinavian_name_service/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import stati
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('name_correction.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
